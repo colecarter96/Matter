b4b6c0bdf019e41000714d24f8053ecc36457090
Cole <colescarter@gmail.com>
2025-02-04 15:05:58 -0800

    Does this work?

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
-import About from "./components/About";
 
 export default function Home() {
   return (
