0d3025cad8caed254555ec8e9f633b7a0304870f
Cole <colescarter@gmail.com>
2025-02-04 15:04:07 -0800

    fixed error for deployment

--- my-app/app/TradingAutomation/page.tsx
+++ my-app/app/TradingAutomation/page.tsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import About from "../components/About";
 import Image from "next/image";
 
 const TradingAutomationPage = () => {
