ba7acc74409639a7a1f0626e01ee49c7bcf8a434
Cole <colescarter@gmail.com>
2025-02-05 20:24:42 -0800

    Header changes

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -4,10 +4,10 @@ const Header = () => {
 
     return(
         <div>
-            <div className="bg-black">
+            <div className=" bg-black">
                 asdf
             </div>
-            <header className="flex justify-center items-center pt-4 pb-4 px-6 bg-white ">
+            <header className="flex justify-center items-center pt-4 pb-4 px-6 text-white bg-black lg:bg-white lg:text-black">
             
             <h1 className="text-3xl font-bold">
             <Link href="/">Matter.</Link>
