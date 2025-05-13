0fa7ca9464192b8109babc6d03fd18d27bc9d5d4
liamkmathers <liamkmathers@gmail.com>
2025-05-13 21:04:25 +0200

    Update eslint.config.mjs

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -1,6 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextEslint from '@eslint/eslintrc'
+import eslintJs from '@eslint/js'
+// @ts-expect-error
+import eslintConfigNext from 'eslint-config-next'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +15,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      // Disable the rule enforcing entity escaping for apostrophes and quotes in JSX
+      "react/no-unescaped-entities": "off"
+    }
+  }),
+  ...eslintJs.configs.recommended
 ];
 
 export default eslintConfig;
